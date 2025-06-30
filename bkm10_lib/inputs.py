be5c4fa6bacf282a8e968b7a0d20bc51d0567002
MidnightExcursion <dima.watkins@gmail.com>
2025-06-30 08:47:12 -0500

    some commentary on the bkm10 inputs --- looks like it was not exposed to user!

--- bkm10_lib/inputs.py
+++ bkm10_lib/inputs.py
@@ -1,19 +1,32 @@
 """
+Entry point for the BKM10Inputs dataclass.
 """
 
 # (1): Import the specialized `dataclass` library:
 from dataclasses import dataclass
 
+# (2): Define the dataclass right away:
 @dataclass
 class BKM10Inputs:
-    # (X): Q^{2}: photon virtuality:
+    """
+    Welcome to the `BKKM10Inputs` dataclass! 
+
+    ## Description:
+    We use this dataclass to handle the passage of the 
+    "kinematic settings" that parametrize the DVCS cross section 
+    according to the BKM10 formalism. That means, in order to
+    obtain a cross section at the end of your calculation, you 
+    must instantiate this dataclass.
+    """
+
+    # (1): Q^{2}: photon virtuality:
     squared_Q_momentum_transfer: float
 
-    # (X): x_{B}: Bjorken's x:
+    # (2): x_{B}: Bjorken's x:
     x_Bjorken: float
 
-    # (X): t: hadron momentum transfer: (p - p')^{2}:
+    # (3): t: hadron momentum transfer: (p - p')^{2}:
     squared_hadronic_momentum_transfer_t: float
 
-    # (X): ...
+    # (4): ...
     lab_kinematics_k: float
