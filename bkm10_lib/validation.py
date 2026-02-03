1cc78d4c35581c2582910084d99155dd2dc44c4a
MidnightExcursion <dima.watkins@gmail.com>
2026-02-03 17:21:59 -0600

    some updates to the validation --- we will edit it a bit more later

--- bkm10_lib/validation.py
+++ bkm10_lib/validation.py
@@ -1,39 +1,51 @@
 """
-
+Basic validation function for the `DifferentialCrossSection` library.
 """
 
 from bkm10_lib.inputs import BKM10Inputs
 from bkm10_lib.cff_inputs import CFFInputs
 
-def validate_configuration(configuration_dictionary: dict, verbose: bool):
+def validate_configuration(
+        configuration_dictionary: dict,
+        verbose: bool):
     """
     ## Description:
     Validate user's dict of initialization parameters.
     
-    ## Parameters:
-    configuration_dictionary (dict):
-        a dict of user-requests DNN parameters that 
-        will be used to attempt to build a TF network.
+    :param dict configuration_dictionary:
+        Dictionary of configuration parameters/inputs required
+        to evaluate d^{4}\sigma in the BKM10 formalism.
 
-    verbose (boolean):
+    :param bool verbose: 
         Do you want to see all output of this function evaluation?
 
-    ## Notes
-    
+    ## Notes:
+        1. 2026/02/03
+            - Removed `lepton_helicity` and `target_polarization`.
     """
 
     required_keys = [
         "kinematics",
         "cff_inputs",
-        "target_polarization", 
-        "lepton_beam_polarization"
         ]
     
+    # (X): ...
+    if verbose:
+
+        # (X): ...
+        print("> [VERBOSE]: Now iterating over required keys...")
+        
     for key in required_keys:
         if key not in configuration_dictionary:
             raise ValueError(f"Missing required key in config: {key}")
     
     kinematic_settings = configuration_dictionary["kinematics"]
+
+    # (X): ...
+    if verbose:
+
+        # (X): ...
+        print("> [VERBOSE]: Extracted kinematics from configuration dictionary.")
 
     if not kinematic_settings:
         raise ValueError("> Missing 'kinematics' key.")
@@ -43,26 +55,16 @@ def validate_configuration(configuration_dictionary: dict, verbose: bool):
 
     cff_settings = configuration_dictionary["cff_inputs"]
 
+    # (X): ...
+    if verbose:
+
+        # (X): ...
+        print("> [VERBOSE]: Extracted CFF settings from configuration dictionary.")
+
     if not cff_settings:
         raise ValueError("> Missing 'cff_inputs' key.")
 
     if not isinstance(cff_settings, CFFInputs):
         raise TypeError("> 'cff_inputs' key must be a CFFInputs instance.")
-    
-    target_polarization = configuration_dictionary["target_polarization"]
-
-    if target_polarization is None:
-        raise ValueError("> Missing 'target_polarization' key.")
-
-    if target_polarization != 0. and target_polarization != 0.5 and target_polarization != -0.5:
-        raise TypeError("> 'target_polarization' key must be a *float* of -1.0, 0.0, or 1.0 only.")
-    
-    lepton_beam_polarization = configuration_dictionary["lepton_beam_polarization"]
-
-    if lepton_beam_polarization is None:
-        raise ValueError("> Missing 'lepton_beam_polarization' key.")
-
-    if lepton_beam_polarization != 0. and lepton_beam_polarization != 1.0 and lepton_beam_polarization != -1.0:
-        raise TypeError("> 'lepton_beam_polarization' key must be a *float* of -0.5, 0.0, or 0.5 only.")
     
     return configuration_dictionary
